c880faef4a5a8cd7282f20bfd5ff6500834d5fc5
荒井章伍 <arais@araiakiragonoiMac.local>
2023-04-27 20:31:27 +0900

    add css default settings

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -6,27 +6,25 @@ import Title from "../components/title/Title";
 
 function Index() {
   return (
-    <>
-      <div className={styles.root}>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className={styles.logo} alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img
-              src={reactLogo}
-              className={`${styles.logo} ${styles.react}`}
-              alt="React logo"
-            />
-          </a>
-        </div>
-        <Title title={"template"} size={"xl"} status={"primary"} />
-        <p className={styles.readTheDocs}>
-          A template using vite with react, jest, storybook, type script, scss,
-          and css modules.
-        </p>
+    <div className={styles.index}>
+      <div className={styles.index__logo}>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img
+            className={styles.index__react}
+            src={reactLogo}
+            alt="React logo"
+          />
+        </a>
       </div>
-    </>
+      <Title title={"template"} size={"xl"} status={"primary"} />
+      <p className={styles.index__text}>
+        A template using vite with react, jest, storybook, type script, scss,
+        and css modules.
+      </p>
+    </div>
   );
 }
 
